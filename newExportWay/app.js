6e35584d1a403fd80a708d7deb5748a1a330235e
Miquel Vidal <miquel@miquelvidal.com>
2022-05-01 19:40:36 +0200

    Fail export import new way modules work https://stackoverflow.com/questions/58211880/uncaught-syntaxerror-cannot-use-import-statement-outside-a-module-when-import

---
+++ newExportWay/app.js
@@ -0,0 +1,5 @@
+import * as greetr from 'greet';
+greetr.greet();
+// no funciona pq estem en carpetes dins una cosa més grosa
+// hauria de ser un joc de carpetes propies
+//https://stackoverflow.com/questions/58211880/uncaught-syntaxerror-cannot-use-import-statement-outside-a-module-when-import

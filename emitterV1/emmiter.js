5aa56b4c90c418e84b5eefcad5dc093919413c0f
Miquel Vidal <miquel@miquelvidal.com>
2022-05-10 23:18:50 +0200

    v3 emitter, amb inici config module amb core events

---
+++ emitterV1/emmiter.js
@@ -0,0 +1,18 @@
+function Emitter() {
+    this.events = {
+        prop: [];
+    };
+}
+
+Emitter.prototype.on = function(type, listener){
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+}
+
+Emitter.prototype.emit = function(type) {
+    if(this.events[type]){
+        this.events[type].array.forEach(element => {
+            element.listener();
+        });
+    }
+}
